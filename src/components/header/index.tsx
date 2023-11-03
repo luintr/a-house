e55399da72ec5929093449af4362e1ad4be63591
luintr <hainam.huflit@gmail.com>
2023-11-04 00:14:32 +0700

    feat: init logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import s from "./styles.module.scss";
 import { AnimatePresence } from "framer-motion";
 import Navbar from "@components/nav";
 import { useNavState } from "@/store/store";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@images/logo_hori.png";
 
 const Header = () => {
   const { isActive, setIsActive } = useNavState();
@@ -14,6 +17,15 @@ const Header = () => {
         className={`${s.backdrop} ${isActive ? s.open : ""}`}
         onClick={setIsActive}
       ></div>
+
+      <Link href={"/"} className={s.logo}>
+        <Image
+          width={Logo.width}
+          height={Logo.height}
+          src={Logo.src}
+          alt={"Logo"}
+        />
+      </Link>
       <div
         onClick={setIsActive}
         className={`${s.hamburger} ${isActive ? s.active : ""}`}
