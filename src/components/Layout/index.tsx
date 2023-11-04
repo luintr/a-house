081ee1160200a106d1c9e9d6b831e4c0242e8d4f
anhtuanlee <anhtuan.freelc@gmail.com>
2023-11-04 15:55:34 +0700

    feat: complete cursor sticky

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 
 	return (
 		<Fragment>
-			<Header />
+			<Header ref={stickyElement} />
 			<Cursor stickyElement={stickyElement} />
 			{children}
 		</Fragment>
